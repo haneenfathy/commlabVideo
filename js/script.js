ba54da996b35003840db32f3a9c3c1dd801eed12
Haneen Fathy <hf953@nyu.edu>
2020-05-12 01:56:12 +0200

    Finished first 2 pages

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+window.onload=function(){
+	
+
+
+  	var input = document.getElementById("myInput");
+	input.addEventListener("keyup", function(event) {
+		console.log("here");
+	  if (event.keyCode === 13) {
+	  	console.log("here again");
+	   document.getElementById("myBtn").click();
+	  }
+	});
+
+}
+function openNav() {
+	console.log("open");
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+var i = 0;
+var txt = 'WELCOME PLEASE SCAN FOR SECURITY CLEARANCE. ';
+var speed = 150;
+
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+
+function data() {
+
+	
+	var name = "  "+ document.getElementById("myInput").value;
+	document.getElementById("data").innerHTML = name.toUpperCase();
+	document.getElementById("myInput").value = " ";
+
+}
+
+
+
+
+
+
+
